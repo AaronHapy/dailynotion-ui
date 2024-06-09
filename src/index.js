3e9ee019d1c754e5ae09de03e0b940c2cd67c583
AaronHapy <aaroncerceda26@gmail.com>
2024-06-09 14:03:58 -0500

    adds watch functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {createBrowserRouter, createRoutesFromElements, Route ,RouterProvider} from 'react-router-dom';
 import App from './App';
 import Home from './screens/Home';
+import Watch from './screens/Watch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<Home />} />
+      <Route path='/watch' element={<Watch />} />
     </Route>
   )
 );
--- src/utlis/mocks/mock.js
+++ src/utlis/mocks/mock.js
@@ -6,10 +6,80 @@ export const fixtures = [
         thumbnail: 'https://i.ytimg.com/vi/bHOHGz87_38/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgTIEEofzAP&rs=AOn4CLCTp1e2MDCK49EXgtSTKwLARQ9CjA',
         channel: 'MondoMedia',
         channelPic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+        channelSubscribersCount: 32000,
         likesCount: 71000,
         views: 200000,
         uploadedDate: '11/22/2020',
-        description: 'Episodes, games, and contests visit: http://www.mondomedia.com',
+        description: "Learn how to improve the speed and performance of your Spring Boot application by implementing Redis Cache. In this video, we will guide you through the process of integrating Redis Cache into your Spring Boot project to boost your app's speed and efficiency. Redis Cache is a powerful tool that can help optimize the performance of your application by storing frequently accessed data in memory. Watch this tutorial and take your Spring Boot application to the next level with Redis Cache!",
+        comments: [
+            {
+                username: '@aaroncerceda',
+                profilePic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+                createdComment: '11/22/2020',
+                likesCount: 20,
+                content: 'When the animator was too lazy to animate fully cuddles'
+            }
+        ]
+    },
+
+    {
+        id: 'qZWPXPovkGI',
+        title: '1000th Upload: Happy Tree Friends Holiday Marathon',
+        source: './path',
+        thumbnail: 'https://i.ytimg.com/vi/bHOHGz87_38/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgTIEEofzAP&rs=AOn4CLCTp1e2MDCK49EXgtSTKwLARQ9CjA',
+        channel: 'MondoMedia',
+        channelPic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+        channelSubscribersCount: 32000,
+        likesCount: 71000,
+        views: 200000,
+        uploadedDate: '11/22/2020',
+        description: "Learn how to improve the speed and performance of your Spring Boot application by implementing Redis Cache. In this video, we will guide you through the process of integrating Redis Cache into your Spring Boot project to boost your app's speed and efficiency. Redis Cache is a powerful tool that can help optimize the performance of your application by storing frequently accessed data in memory. Watch this tutorial and take your Spring Boot application to the next level with Redis Cache!",
+        comments: [
+            {
+                username: '@aaroncerceda',
+                profilePic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+                createdComment: '11/22/2020',
+                likesCount: 20,
+                content: 'When the animator was too lazy to animate fully cuddles'
+            }
+        ]
+    },
+
+    {
+        id: 'qZWPXPovkGI',
+        title: '1000th Upload: Happy Tree Friends Holiday Marathon',
+        source: './path',
+        thumbnail: 'https://i.ytimg.com/vi/bHOHGz87_38/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgTIEEofzAP&rs=AOn4CLCTp1e2MDCK49EXgtSTKwLARQ9CjA',
+        channel: 'MondoMedia',
+        channelPic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+        channelSubscribersCount: 32000,
+        likesCount: 71000,
+        views: 200000,
+        uploadedDate: '11/22/2020',
+        description: "Learn how to improve the speed and performance of your Spring Boot application by implementing Redis Cache. In this video, we will guide you through the process of integrating Redis Cache into your Spring Boot project to boost your app's speed and efficiency. Redis Cache is a powerful tool that can help optimize the performance of your application by storing frequently accessed data in memory. Watch this tutorial and take your Spring Boot application to the next level with Redis Cache!",
+        comments: [
+            {
+                username: '@aaroncerceda',
+                profilePic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+                createdComment: '11/22/2020',
+                likesCount: 20,
+                content: 'When the animator was too lazy to animate fully cuddles'
+            }
+        ]
+    },
+
+    {
+        id: 'qZWPXPovkGI',
+        title: '1000th Upload: Happy Tree Friends Holiday Marathon',
+        source: './path',
+        thumbnail: 'https://i.ytimg.com/vi/bHOHGz87_38/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgTIEEofzAP&rs=AOn4CLCTp1e2MDCK49EXgtSTKwLARQ9CjA',
+        channel: 'MondoMedia',
+        channelPic: 'https://yt3.ggpht.com/ytc/AIdro_lrjNMswyLSqpdQfzf3iKJnAtPda72UlM2XT_2yURHObbc=s48-c-k-c0x00ffffff-no-rj',
+        channelSubscribersCount: 32000,
+        likesCount: 71000,
+        views: 200000,
+        uploadedDate: '11/22/2020',
+        description: "Learn how to improve the speed and performance of your Spring Boot application by implementing Redis Cache. In this video, we will guide you through the process of integrating Redis Cache into your Spring Boot project to boost your app's speed and efficiency. Redis Cache is a powerful tool that can help optimize the performance of your application by storing frequently accessed data in memory. Watch this tutorial and take your Spring Boot application to the next level with Redis Cache!",
         comments: [
             {
                 username: '@aaroncerceda',
